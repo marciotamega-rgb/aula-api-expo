81333fed3cb62095a00db33a3d2175f39f345b5c
marciotamega-rgb <marciotamega@gmail.com>
2026-08-17 20:17:57 -0300

    atualizacoes
    17/08/26

--- src/routes/rootStack.js
+++ src/routes/rootStack.js
@@ -9,11 +9,20 @@ const Stack =createNativeStackNavigator();
 
 export default function rootStack() {
   return (
-    <Stack.Navigator>
-    <Stack.Screen name="Login" component={Login}  />
-    <Stack.Screen name="Login" component={homescreen}  />
-    <Stack.Screen name="Login" component={cadusuario}  />
-    <Stack.Screen name="Login" component={recsenha}  />
+    <Stack.Navigator initialRouteName="login">  
+    <Stack.Screen 
+    name="Login"
+     component={Login}  
+     options={{title: 'login'}}
+     />
+    <Stack.Screen 
+    name="cadusuario"
+     component={cadusuario}  />
+       options={{title: 'cadastro de usuário'}}
+    <Stack.Screen 
+    name="recsenha"
+     component={recsenha}  />
+       options={{title: 'recuperação de senha'}}
     </Stack.Navigator>
   );
 }
